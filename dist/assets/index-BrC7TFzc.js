9810c54c645531c37be944bfcd9f22460f987496
zyx <1300717503@qq.com>
2024-11-25 19:56:35 +0800

    fix:github page 部署

---
+++ dist/assets/index-BrC7TFzc.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+    
+import{a1 as f}from"./index-BgC0ENBM.js";export{f as default};
---
+++ dist/assets/index-sFISNk9U.js
@@ -0,0 +1,8 @@
+
+/**
+ * 由 Fantastic-admin 提供技术支持
+ * Powered by Fantastic-admin
+ * https://fantastic-admin.hurui.me
+ */
+    
+import{ar as r}from"./index-BgC0ENBM.js";export{r as default};
