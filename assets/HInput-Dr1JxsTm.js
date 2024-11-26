c386d359d10df8a86aaa7f4b931771aab8234d90
zyx <1300717503@qq.com>
2024-11-26 20:53:59 +0800

    fix:部署时，mock接口的前缀问题

--- assets/HInput-Dr1JxsTm.js
+++
@@ -1,8 +0,0 @@
-
-/**
- * 由 Fantastic-admin 提供技术支持
- * Powered by Fantastic-admin
- * https://fantastic-admin.hurui.me
- */
-    
-import{_ as m}from"./HInput.vue_vue_type_script_setup_true_lang-C3bLCb4w.js";import"./index-hvzRevkC.js";export{m as default};
